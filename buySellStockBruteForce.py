46042e6e6bf791d243a0fba4aaf2d13d3cc73706
Kapil Dev <kapil.dev87@gmail.com>
2019-07-26 08:41:26 -0700

    Create buySellStockBruteForce.py
    Buy-Sell Stocks brute force solution

---
+++ buySellStockBruteForce.py
@@ -0,0 +1,23 @@
+def calculate(prices, s):
+    if s >= len(prices):
+        #print 's', s
+        return 0
+
+    maxProfitSoFar = 0
+    #print 'maxProfitSoFar', maxProfitSoFar
+    for start in range(s, len(prices)):
+        maxProfit = 0
+        #print 'maxProfit', maxProfit
+        for i in range(s+1, len(prices)):
+            if prices[start] < prices[i]:
+                profit = calculate(prices, i+1) + prices[i] - prices[start]
+                if profit > maxProfit:
+                    print 'start: %s, i: %s'%(start, i)
+                    maxProfit = profit
+        if maxProfit > maxProfitSoFar:
+            maxProfitSoFar = maxProfit
+            print 'start: %s maxProfitSoFar: %s'%(start, maxProfitSoFar)
+
+    return maxProfitSoFar
+
+return calculate(prices, 0)
