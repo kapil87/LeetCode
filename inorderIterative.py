da21339d9400a7df1665e3909b2849e8b7f4ab58
Kapil Dev <kapil.dev87@gmail.com>
2019-08-16 19:27:02 -0700

    Update inorderIterative.py
    Inorder traversal using a stack, Leetcode iterative solution.

--- inorderIterative.py
+++ inorderIterative.py
@@ -13,3 +13,33 @@ def inorderIterative(tree):
             #Go to the right subtree
             tree = tree.right
     return result
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        #Root, left, right
+        if not root:
+            return []
+        treeElements = []
+        stack = []
+        curr = root
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            treeElements.append(curr.val)
+            curr = curr.right
+            
+        return treeElements
+        
