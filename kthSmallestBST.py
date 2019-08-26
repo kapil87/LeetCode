2a58c68fb2d15d5aba1fe267501c0bd019545706
Kapil Dev <kapil.dev87@gmail.com>
2019-08-26 08:31:19 -0700

    Update kthSmallestBST.py
    Adding an iterative solution.
    #Simple iterative solution with O(h+k) where we keep the h+k nodes in the stack. In worst case wen have O(n) time complexity

--- kthSmallestBST.py
+++ kthSmallestBST.py
@@ -12,8 +12,22 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        '''
         #Simple inorder traversal solution with O(n) time and space
         def inorder(root):
             return inorder(root.left) + [root.val] + inorder(root.right) if root else []
         
         return inorder(root)[k-1]
+        '''
+        #Simple iterative solution with O(h+k) where we keep the h+k nodes in the stack. In worst case wen have O(n) time complexity 
+        stack = []
+        
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -=1
+            if not k:
+                return root.val
+            root = root.right
