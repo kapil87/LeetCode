a01a6c6b15fe1adf484eb96cd4533d6ff5d9d9d1
Kapil Dev <kapil.dev87@gmail.com>
2019-08-27 20:49:13 -0700

    Update kthSmallestMatrix.py
    Interesting solution based on graph/tree structure which uses the fact that rows and columns are sorted.
    The solution is base on following link https://medium.com/brain-framework/kth-smallest-element-in-sorted-matrix-b20400cf878e

--- kthSmallestMatrix.py
+++ kthSmallestMatrix.py
@@ -7,6 +7,7 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        '''
         heap = []
         
         for row in matrix:
@@ -17,4 +18,23 @@ class Solution(object):
                     heapq.heappop(heap)
                     
         return -1*heapq.heappop(heap)
+        '''
+                s = set()
+        s.add((0,0))
+        heap = [(matrix[0][0], 0, 0)]
         
+        while k > 1:
+            top = heapq.heappop(heap)
+            row, col = top[1], top[2]
+            
+            if col+1 < len(matrix[0]) and (row, col+1) not in s:
+                heapq.heappush(heap, (matrix[row][col+1], row, col+1))
+                s.add((row, col+1))
+            
+            if row+1 < len(matrix) and (row+1, col) not in s:
+                heapq.heappush(heap, (matrix[row+1][col], row+1, col))
+                s.add((row+1, col))
+            
+            k -=1
+        
+        return heap[0][0]
