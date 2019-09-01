0f19b3e0ed81a4cae2cc26b69580ef6b1a9158a7
Kapil Dev <kapil.dev87@gmail.com>
2019-08-31 19:47:58 -0700

    Create reorganizeString.py
    A simple pythonic solution to re-organize strings.

---
+++ reorganizeString.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def reorganizeString(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+        N = len(S)
+        A = []
+        for c, x in sorted((S.count(x), x) for x in set(S)):
+            if c > (N+1)/2: return ""
+            print c, x
+            A.extend(c * x)
+        ans = [None] * N
+        ans[::2], ans[1::2] = A[N/2:], A[:N/2]
+        return "".join(ans)
