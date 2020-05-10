8310163a5761143967d227f390a41e2128963300
Kapil Dev <kapil.dev87@gmail.com>
2020-05-10 09:09:33 -0700

    Update kClosest.py
    Intuition
    
    We want an algorithm faster than N \log NNlogN. Clearly, the only way to do this is to use the fact that the K elements returned can be in any order -- otherwise we would be sorting which is at least N \log NNlogN.
    
    Say we choose some random element x = A[i] and split the array into two buckets: one bucket of all the elements less than x, and another bucket of all the elements greater than or equal to x. This is known as "quickselecting by a pivot x".
    
    The idea is that if we quickselect by some pivot, on average in linear time we'll reduce the problem to a problem of half the size.

--- kClosest.py
+++ kClosest.py
@@ -5,7 +5,47 @@ class Solution(object):
         :type K: int
         :rtype: List[List[int]]
         """
-        points.sort(key = lambda P: P[0]**2 + P[1]**2)
+        #points.sort(key = lambda P: P[0]**2 + P[1]**2)
+        #return points[:K]
+    
+        dist = lambda i: points[i][0]**2 + points[i][1]**2 
+        
+        def sort(i, j, K):
+            # Partially sorts A[i:j+1] so that first K elements are 
+            # the smallest K elements
+            if i >= j: return 
+            
+            # Put random elements as A[i] - this is the pivot
+            k = random.randint(i, j)
+            points[i], points[k] = points[k], points[i]
+            
+            mid = partition(i, j)
+            if K < mid - i + 1:
+                sort(i, mid - 1, K)
+            elif K > mid - i + 1:
+                sort(mid + 1, j, K - (mid - i + 1))
+        
+        def partition(i, j):
+            # Partition by pivot A[i], returning and index mid
+            # such that A[i] <= A[mid] <= A[j] for i < mind < j
+            oi = i
+            pivot = dist(i)
+            i += 1
+            
+            while True:
+                while i < j and dist(i) < pivot:
+                    i += 1
+                while i <= j and dist(j) >= pivot:
+                    j -= 1
+                if i >= j: break
+                points[i], points[j] = points[j], points[i]
+            
+            points[oi], points[j] = points[j], points[oi]
+            return j
+        
+            
+        sort(0, len(points) -1, K)
+        print(points)
         return points[:K]
         
         
